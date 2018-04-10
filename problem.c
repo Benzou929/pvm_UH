46760d1ec243d781b2d66d97d619473a84770dee
Benzou929 <33153112+Benzou929@users.noreply.github.com>
2018-04-10 15:20:48 +0200

    Add files via upload

---
+++ problem.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <malloc.h>
+#include "pvm3.h"
+#include "point.h"
+
+
+void send_pb(int tid, pb_t *pb)
+{
+	int *tabX,*tabY;
+	int counter = 0;
+	pb_t *pb_local= pb_t;
+	while(pb_local->next != NULL){
+	  pb_local = pb_local-> next;
+	  counter ++;
+	}
+	tabX = (int *) malloc(counter*sizeof(int));
+	tabY = (int *) malloc(counter*sizeof(int));
+	int i=0;
+	
+	while(pb->next != Null){
+	 tabX[i] = pb->x;
+	 tabY[i] = pb->y;
+	 i++;
+	}
+	
+	
+
+	pb_free(pb);
+}
+
+
+
+
